f6c5dbc836dbd87fe80383219ac610a5a62a00ff
Jad229 <jad229@njit.edu>
2022-11-13 18:25:49 -0500

    Updates

---
+++ precache-manifest.00287b8c067c9b9d289cf3dcdd1a34e9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde3f3bceda438dca357784cd400dce7",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "cdea3ee903c4cc19f467",
+    "url": "/keeper-app/static/js/2.df557289.chunk.js"
+  },
+  {
+    "revision": "f692422d3e2c32f3dc95",
+    "url": "/keeper-app/static/js/main.ced66076.chunk.js"
+  },
+  {
+    "revision": "3f5fcd70531cb0f5d766",
+    "url": "/keeper-app/static/js/runtime-main.ad8fc91b.js"
+  }
+]);
